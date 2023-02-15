cf9425699f7a4bd2f621f37d7d3fc240a1999b2b
jaysonocharo <mogakajayson@gmail.com>
2023-02-15 11:54:21 +0300

    added calculator functions

--- calculator.py
+++ calculator.py
@@ -5,16 +5,27 @@ operator = input("Enter operator:")
 
 def add(num1,num2):
     result = num1 + num2
-    return result
+    print(result)
 
 def subtract(num1,num2):
     result = num1 - num2
-    return result
+    print(result)
 
 def divide(num1,num2):
     result = num1 / num2
-    return result
+    print(result)
 
 def multiply(num1,num2):
     result = num1 * num2
-    return result
+    print(result)
+
+if operator == "+":
+    add(number1,number2)
+elif operator == "-":
+    subtract(number1,number2)
+elif operator == "/":
+    divide(number1,number2)
+elif operator == "X" or operator == "x" or operator == "*":
+    multiply(number1,number2)
+else:
+    print("Invalid operator")
